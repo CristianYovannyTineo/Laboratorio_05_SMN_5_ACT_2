cd060d80ca008e61f1477b4396c07b951a209d05
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-09-20 23:15:58 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio_05_SMN_5_ACT_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Escribir un programa que permita hallar la potencia de un numero ingresando la base y el exponente 
+
+    int Resultado = 1;
+    Console.Write("Ingrese la Base: ");
+     int BaseNumero = int.Parse(Console.ReadLine());
+    Console.Write("Ingrese el Exponente: ");
+     int exponente = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < Math.Abs(exponente); i++)
+    {
+        Resultado *= BaseNumero;
+    }
+    if (exponente < 0)
+    {
+        Resultado = 1 / Resultado;
+    }
+    Console.WriteLine("La potencia es: " + Resultado);
